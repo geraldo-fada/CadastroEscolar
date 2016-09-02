753f576b24c7a13641c7e2876f5505d67b706b24
Geraldo Fada <geraldofada.neto@gmail.com>
2016-09-02 01:24:05 -0300

    Consulta de professor concluida

---
+++ controllers/updaters/consultaProfessorControllerUpdater.php
@@ -0,0 +1,42 @@
+<?php
+  require_once '../dbController.php';
+
+  if(isset($_POST["pesquisa"]) && isset($_POST["campo"])) {
+    $pesquisa = utf8_decode($_POST["pesquisa"]);
+    $campo = utf8_decode($_POST["campo"]);
+
+    try {
+      $professores = $con->query("SELECT professores.cpf, nome, idade, turma_nome, disciplina_nome " .
+                                 "FROM professores, professores_turmas, professores_disciplinas " .
+                                 "WHERE " . $campo . " LIKE '" . $pesquisa . "%' " .
+                                 "AND professores.cpf = professores_turmas.professor_cpf " .
+                                 "AND professores.cpf = professores_disciplinas.professor_cpf ");
+
+      echo "<table>
+              <tr>
+                <th>CPF</th>
+                <th>Nome</th>
+                <th>Idade</th>
+                <th>Turma</th>
+                <th>Disciplina</th>
+                <th>Ações</th>
+              </tr>";
+                foreach($professores as $professor) {
+                  echo "<tr>";
+                  echo "<td>" . utf8_encode($professor[0]) . "</td>";
+                  echo "<td>" . utf8_encode($professor[1]) . "</td>";
+                  echo "<td>" . utf8_encode($professor[2]) . "</td>";
+                  echo "<td>" . utf8_encode($professor[3]) . "</td>";
+                  echo "<td>" . utf8_encode($professor[4]) . "</td>";
+                  echo "<td>
+                          <i class='fa fa-pencil' aria-hidden='true'></i>
+                          <i class='fa fa-trash' aria-hidden='true'></i>
+                        </td>";
+                  echo "</tr>";
+                }
+      echo "</table>";
+    }
+    catch(PDOException $e) {
+      echo "Error: " . $e;
+    }
+  }
