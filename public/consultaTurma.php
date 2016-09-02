599c090b04907ce3e977e66ad128b5394930a091
Geraldo Fada <geraldofada.neto@gmail.com>
2016-09-02 01:31:17 -0300

    Consulta de turmas concluida

---
+++ public/consultaTurma.php
@@ -0,0 +1,23 @@
+<?php require_once "../templates/headerTemplate.php"; ?>
+
+      <h1>Consultar turmas</h1>
+
+      <form id="consTurma" method="post" action="">
+
+        <!-- PESQUISAR POR -->
+        <div class="form-input">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          <!-- INPUT -->
+          <input type="text" name="pesquisa" maxlength="6" placeholder="Pesquisar por..." size="50"
+          onblur="consultarCampo('Turma');">
+
+          <select name="campo">
+            <option value="nome">Nome</option>
+            <option value="tudo">Tudo</option>
+          </select>
+        </div>
+        <div id="query_validate_check"></div>
+
+      </form>
+
+<?php require_once "../templates/footerTemplate.php" ?>
