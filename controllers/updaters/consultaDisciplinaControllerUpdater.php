b3e05ea7f4038f74b0c238830c41a7d4db781a44
Geraldo Fada <geraldofada.neto@gmail.com>
2016-09-02 01:37:27 -0300

    Consulta de disciplinas concluido

---
+++ controllers/updaters/consultaDisciplinaControllerUpdater.php
@@ -0,0 +1,34 @@
+<?php
+  require_once '../dbController.php';
+
+  if(isset($_POST["pesquisa"]) && isset($_POST["campo"])) {
+    $pesquisa = utf8_decode($_POST["pesquisa"]);
+    $campo = utf8_decode($_POST["campo"]);
+
+    try {
+      if ($campo == "tudo") {
+        $disciplinas = $con->query("SELECT * FROM disciplinas ");
+      }
+      else {
+        $disciplinas = $con->query("SELECT * FROM disciplinas WHERE " . $campo . " LIKE '" . $pesquisa . "%' ");
+      }
+
+      echo "<table>
+              <tr>
+                <th>Nome</th>
+                <th>Ações</th>
+              </tr>";
+                foreach($disciplinas as $disciplina) {
+                  echo "<tr>";
+                  echo "<td>" . utf8_encode($disciplina[0]) . "</td>";
+                  echo "<td>
+                          <i class='fa fa-trash' aria-hidden='true'></i>
+                        </td>";
+                  echo "</tr>";
+                }
+      echo "</table>";
+    }
+    catch(PDOException $e) {
+      echo "Error: " . $e;
+    }
+  }
---
+++ public/consultaDisciplina.php
@@ -0,0 +1,23 @@
+<?php require_once "../templates/headerTemplate.php"; ?>
+
+      <h1>Consultar disciplinas</h1>
+
+      <form id="consDisciplina" method="post" action="">
+
+        <!-- PESQUISAR POR -->
+        <div class="form-input">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          <!-- INPUT -->
+          <input type="text" name="pesquisa" maxlength="10" placeholder="Pesquisar por..." size="50"
+          onblur="consultarCampo('Disciplina');">
+
+          <select name="campo">
+            <option value="nome">Nome</option>
+            <option value="tudo">Tudo</option>
+          </select>
+        </div>
+        <div id="query_validate_check"></div>
+
+      </form>
+
+<?php require_once "../templates/footerTemplate.php" ?>
